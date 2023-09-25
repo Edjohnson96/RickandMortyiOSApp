4eca9c6ea2226bd05bf084497d2f9504e35f5326
Jack Tracy <info@jacktracy.co.uk>
2023-09-25 17:03:07 +0100

    Added doc strings and api related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Ed Johnson on 25/09/2023.
+//
+
+import Foundation
+
+/// Object that represents a single API Call
+final class RMRequest {
+    
+}
